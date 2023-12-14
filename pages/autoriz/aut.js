e55844fd8a39e62d0bddad331411afc9a94bc8a4
osip123 <131646261+osip123@users.noreply.github.com>
2023-12-14 11:10:04 +0300

    first commit

---
+++ pages/autoriz/aut.js
@@ -0,0 +1,20 @@
+
+
+
+var pass = document.querySelector('.mail')
+var mail = document.querySelector('.pass')
+var sbtn = document.querySelector('.btn')
+
+
+
+passes = [
+
+]
+
+function checkPass(){
+    alert("hellow")
+}
+
+checkPass();
+
+console.log(pass, mail)
---
+++ pages/src/script.js
@@ -0,0 +1,19 @@
+var pass = document.getElementById('pass')
+var mail = document.getElementById('mail')
+var btn = document.getElementById('ayt')
+
+btn.addEventListener('click') = () => {
+    console.log('1')
+}
+
+const theme = document.getElementById('root')
+
+theme.onclick = () => {
+    var themeClass = document.getElementById('root').classList
+    themeClass.className += ' newClass';
+}
+
+const newBanner = () => {
+    document.getElementsByClassName('topnav')
+    
+}
